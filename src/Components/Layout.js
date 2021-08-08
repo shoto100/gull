57fa7f3ca5c21231a148c6a291cc4e0776e3bac8
SATODHRU NONDON <66862360+shoto2020@users.noreply.github.com>
2021-08-08 21:25:47 +0600

    Gull is ready

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Head from 'next/head'
 import { styled } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
@@ -20,23 +21,26 @@ const Content = styled("div") ({
 
 const Layout = ({ children}) => {
     return (
-      <Box sx={{ flexGrow: 1, }}>
-        <Grid container spacing={0} sx={{height: "100vh" }}>
-          <Grid item xs={3} sm={2} md={1} sx={{backgroundColor: '#663399', textAlign: 'center'}}>
-            <ItemSidebar>
-                <Sidebar></Sidebar>
-              </ItemSidebar>
+      <div>
+        <Head><title>Gull | Dashboard</title></Head>
+        <Box sx={{ flexGrow: 1, }}>
+          <Grid container spacing={0} sx={{height: "100vh" }}>
+            <Grid item xs={3} sm={2} md={1} sx={{backgroundColor: '#663399', textAlign: 'center'}}>
+              <ItemSidebar>
+                  <Sidebar></Sidebar>
+                </ItemSidebar>
+            </Grid>
+            <Grid item xs={9} sm={10} md={11} sx={{backgroundColor: '#f1f3f4',}}>
+              <ItemContent>
+                  <Navbar></Navbar>
+                  <Content>
+                      {children}
+                  </Content>
+              </ItemContent>
+            </Grid>
           </Grid>
-          <Grid item xs={9} sm={10} md={11} sx={{backgroundColor: '#f1f3f4',}}>
-            <ItemContent>
-                <Navbar></Navbar>
-                <Content>
-                    {children}
-                </Content>
-            </ItemContent>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </div>
     );
 };
 
